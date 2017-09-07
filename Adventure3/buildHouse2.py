a1b7ecc25461b626ba6166d9e7e2757be5c02eaf
David Whale <david@thinkingbinaries.com>
2017-09-07 10:51:09 +0100

    Corrections to Adventure 2 code pulled through from chapter tech edit

--- Adventure3/buildHouse2.py
+++ Adventure3/buildHouse2.py
@@ -28,7 +28,7 @@ def house():
     mc.setBlocks(x, y, z, x+SIZE, y+SIZE, z+SIZE, block.COBBLESTONE.id)
     
     # Carve the insides out with AIR    
-    mc.setBlocks(x+1, y, z+1, x+SIZE-2, y+SIZE-1, z+SIZE-2, block.AIR.id)
+    mc.setBlocks(x+1, y, z+1, x+SIZE-1, y+SIZE-1, z+SIZE-1, block.AIR.id)
 
     # Carve out a space for the doorway
     mc.setBlocks(midx-1, y, z, midx+1, y+3, z, block.AIR.id)
--- Adventure3/buildStreet2.py
+++ Adventure3/buildStreet2.py
@@ -29,7 +29,7 @@ def house():
     mc.setBlocks(x, y, z, x+SIZE, y+SIZE, z+SIZE, block.COBBLESTONE.id)
     
     # Carve the insides out with AIR      
-    mc.setBlocks(x+1, y, z+1, x+SIZE-2, y+SIZE-1, z+SIZE-2, block.AIR.id)
+    mc.setBlocks(x+1, y, z+1, x+SIZE-1, y+SIZE-1, z+SIZE-1, block.AIR.id)
 
     # Carve out a space for the doorway
     mc.setBlocks(midx-1, y, z, midx+1, y+3, z, block.AIR.id)
@@ -44,7 +44,7 @@ def house():
     mc.setBlocks(x, y+SIZE, z, x+SIZE, y+SIZE, z+SIZE, block.WOOD.id)
     
     # Add a woolen carpet, the colour is 14, which is red.
-    mc.setBlocks(x+1, y-1, z+1, x+SIZE-2, y-1, z+SIZE-2, block.WOOL.id, 14)
+    mc.setBlocks(x+1, y-1, z+1, x+SIZE-1, y-1, z+SIZE-1, block.WOOL.id, 14)
 
     # Add a random carpet    
     c = random.randint(0, 15)
